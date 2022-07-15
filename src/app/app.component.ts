23b7fa86a6ae60ef7c809df5b278bde9653ccf2b
nightingazer <nightingazer@outlook.com>
2022-07-15 19:07:19 +0200

    setup proxy and standalone component startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrls: ['./app.component.scss'],
+  imports: [CommonModule],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  apiMessage?: string;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http
+      .get<{ message: string }>('api')
+      .subscribe((value) => (this.apiMessage = value.message));
+  }
 }

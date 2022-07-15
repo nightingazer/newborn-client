61e69ff31a7c802f6e1d64940c3c9db5dbb5305b
nightingazer <nightingazer@outlook.com>
2022-07-16 00:15:00 +0200

    add page-not-found component

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router'
+import { HomeComponent, PageNotFoundComponent } from '~core/components'
 
-export const ROUTES: Route[] = []
+export const ROUTES: Route[] = [
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: '**', redirectTo: 'page-not-found' },
+]

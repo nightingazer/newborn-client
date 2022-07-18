0342deca862b77017bb55feb545f696d1876479b
nightingazer <nightingazer@outlook.com>
2022-07-18 18:11:43 +0200

    add translations

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core'
 import { bootstrapApplication } from '@angular/platform-browser'
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app/app.component'
+import { RouterModule } from '@angular/router'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 
 import { environment } from './environments/environment'
-import { RouterModule } from '@angular/router'
 import { ROUTES } from './app/app-routes'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppStoreModule } from './app/features'
 
 if (environment.production) {
@@ -18,5 +21,18 @@ bootstrapApplication(AppComponent, {
     importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
     importProvidersFrom(RouterModule.forRoot(ROUTES)),
     importProvidersFrom(AppStoreModule),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: TranslateHttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
   ],
 }).catch((err) => console.error(err))
+
+function TranslateHttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http)
+}
